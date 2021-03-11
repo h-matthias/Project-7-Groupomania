f2b1cb64bccdb5e3e685b5d51e70d92d18829f25
Hoarau Matthias <h.matthias@live.fr>
2021-03-11 12:56:30 +0400

    dotenv dependence exlu .env et ajout DB app.js

--- API/app.js
+++ API/app.js
@@ -2,6 +2,14 @@ const express = require ( "express");
 const cors = require ( "cors");
 const bodyParser = require ( "body-parser");
 
+const db = require("./entity/models");
+db.sequelize.sync({force: true}).then( () => {
+    console.log("Drop and re-Sync");
+})
+
+//Routes
+const userRoutes = require("./entity/user/user.routes");
+
 
 const app = express();
 app.use(cors());
@@ -10,6 +18,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res ) => res.json({message : "Bienvenue sur le réseau groupomania"}))
+
+app.use("/api/auth", userRoutes);
 
 
 
