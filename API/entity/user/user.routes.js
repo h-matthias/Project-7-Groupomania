6b6d0476210be2cb95e5545a92b87913fc421aca
Hoarau Matthias <h.matthias@live.fr>
2021-03-11 15:04:07 +0400

    modif chemin userCtrl

--- API/entity/user/user.routes.js
+++ API/entity/user/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userCtrl = require ("../user/user.controller");
+const userCtrl = require ("./user.controller");
 
 router.post("/signup", userCtrl.signup);
 router.post("/login", userCtrl.login);
