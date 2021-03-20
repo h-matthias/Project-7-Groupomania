c65466262fa877cabfd2d51f5fb3cffb0dd2fca1
Hoarau Matthias <h.matthias@live.fr>
2021-03-20 11:03:31 +0400

    mise en forme ajout route comment et dep. path

--- API/app.js
+++ API/app.js
@@ -1,33 +1,32 @@
 const express = require ( "express");
+const path = require("path")
 const cors = require ( "cors");
 
 const db = require("./entity/models");
-db.sequelize.sync({force: true}).then( () => {
-    console.log("Drop and re-Sync");
-})
+
+
+// db.sequelize.sync({force: true}).then( () => {
+//     console.log("Drop and re-Sync");
+// })
+
+db.sequelize.sync();
 
 //Routes
 const userRoutes = require("./entity/user/user.routes");
 const postRoutes = require("./entity/post/post.routes");
-
+const commentRoutes = require("./entity/comment/comment.routes")
 
 const app = express();
 app.use(cors());
 
-//remplace bodyParser.json()
 app.use(express.json());
+
+app.use("API/images", express.static(path.join(__dirname, "images")));
 
 app.get("/", (req, res ) => res.json({message : "Bienvenue sur le réseau groupomania"}))
 
 app.use("/api/auth", userRoutes);
-app.use("/api/post", postRoutes),
-
-
-
-
-
-
-
-
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 module.exports = app
