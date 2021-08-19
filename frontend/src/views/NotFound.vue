ee592f161ef943f1007218140ce842d53203637a
h.matthias <h.matthias@live.fr>
2021-08-19 14:56:23 +0400

    page notFound

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <h2 v-if="wrongPath">"{{ wrongPath }}"</h2>
+        <el-button @click="goToPreviousPage" type="primary" round>
+            Revenir en arrière
+        </el-button>
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+import { useRouter, useRoute } from "vue-router";
+export default {
+    setup() {
+        const router = useRouter();
+        const route = useRoute();
+
+        const state = reactive({
+            wrongPath: route.params.wrongPath,
+            title: "Page introuvable",
+        });
+
+        const goToPreviousPage = function() {
+            router.back();
+        };
+
+        return {
+            ...toRefs(state),
+            goToPreviousPage,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
